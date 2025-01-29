72e0af00f3064e02d077a8c9142c6b05135a3510
Dinithi1625403 <dinithi1625403@gmail.com>
2025-01-29 09:13:45 +0530

    initial commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,7 +7,12 @@ export default function Nav(){
     }
     return(
         <nav>
+            <span>
+            <h8 className='navName1'>D</h8>
+            <h8 className='navName2'>INITHI</h8>
+            </span>
             <ul>
+                
                 <li><a className='navLi'onClick={()=>handlescroll("home")} >Home</a></li>
                 <li><a className='navLi'onClick={()=>handlescroll("about")} >About</a></li>
                 <li><a className='navLi'onClick={()=>handlescroll("skills")} >Skills</a></li>
