6c869ecbf798a5e53ef6183ded999e1bfd9f1060
Dinithi1625403 <dinithi1625403@gmail.com>
2025-01-31 17:29:01 +0530

    modify navbar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,12 +6,31 @@ export default function Nav(){
         document.getElementById(id).scrollIntoView({behavior:"smooth"});
     }
     function myFunction() {
+        var z = document.getElementById("navIcon");
+        var y = document.getElementsByClassName("navLi");
         var x = document.getElementById("navUl");
-        if (x.className === "navUl") {
-          x.className = "";
-        } else {
-          x.className = "navUl";  
-    }}
+        x.style.display="block";
+        z.style.display="none";
+        x.style.backgroundColor ="rgba(41, 40, 40, 0.66)";
+        x.style.width = "50%";
+        
+        for (var i = 0; i < y.length; i++) {
+            y[i].style.width = "100%";
+            y[i].style.display = "block";
+            y[i].addEventListener('mouseover', function() {
+                this.style.backgroundColor = 'gray';
+            });
+            y[i].addEventListener('mouseout', function() {
+                this.style.backgroundColor = 'rgba(41, 40, 40, 0.66)';
+            });
+            y[i].style.height = "40px";
+            y[i].style.backgroundColor = "rgba(41, 40, 40, 0.66)";
+            y[i].style.margin = "0px";
+            y[i].style.padding = "0";
+            y[i].style.border = "2px solid black";
+        }
+    }
+   
     return(
         <>
         <nav>
@@ -19,6 +38,8 @@ export default function Nav(){
             <p className='navName1'>D</p>
             <p className='navName2'>INITHI</p>
             </span>
+            
+            <a href="javascript:void(0);" id='navIcon' className="navIcon" onClick={()=>myFunction()}><i className="fa fa-bars"></i></a>
             <ul className='navUl' id='navUl'> 
                 
                 <li><a className='navLi'onClick={()=>handlescroll("home")} >Home</a></li>
@@ -29,7 +50,6 @@ export default function Nav(){
           
 
             </ul>
-            <a href="javascript:void(0);" className="icon" onClick={()=>myFunction()}><i className="fa fa-bars"></i></a>
         </nav>
         </>
     )}
