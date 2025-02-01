b295c9631ceee3870a1572c0601d326e6a4a7c2b
Dinithi1625403 <dinithi1625403@gmail.com>
2025-02-01 11:47:40 +0530

    modify navbar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,29 +5,36 @@ export default function Nav(){
     const handlescroll=(id)=>{
         document.getElementById(id).scrollIntoView({behavior:"smooth"});
     }
+    document.addEventListener('click', function(event) {
+        var isClickInside = document.getElementById('navUl').contains(event.target) || document.getElementById('navIcon').contains(event.target);
+        if (!isClickInside) {
+            document.getElementById('navUl').style.display = 'none';
+            document.getElementById('navIcon').style.display = 'block';
+        }
+    });
     function myFunction() {
         var z = document.getElementById("navIcon");
         var y = document.getElementsByClassName("navLi");
         var x = document.getElementById("navUl");
         x.style.display="block";
         z.style.display="none";
-        x.style.backgroundColor ="rgba(41, 40, 40, 0.66)";
+        x.style.backgroundColor ="rgba(41, 40, 40)";
         x.style.width = "50%";
         
         for (var i = 0; i < y.length; i++) {
-            y[i].style.width = "100%";
+            y[i].style.width = "100vw";
             y[i].style.display = "block";
             y[i].addEventListener('mouseover', function() {
                 this.style.backgroundColor = 'gray';
             });
             y[i].addEventListener('mouseout', function() {
-                this.style.backgroundColor = 'rgba(41, 40, 40, 0.66)';
+                this.style.backgroundColor = 'rgba(41, 40, 40)';
             });
             y[i].style.height = "40px";
-            y[i].style.backgroundColor = "rgba(41, 40, 40, 0.66)";
+           
             y[i].style.margin = "0px";
             y[i].style.padding = "0";
-            y[i].style.border = "2px solid black";
+            
         }
     }
    
@@ -39,7 +46,8 @@ export default function Nav(){
             <p className='navName2'>INITHI</p>
             </span>
             
-            <a href="javascript:void(0);" id='navIcon' className="navIcon" onClick={()=>myFunction()}><i className="fa fa-bars"></i></a>
+            <a href="javascript:void(0);" id='navIcon' className="navIcon"   onClick={()=>myFunction()}><i className="fa fa-bars"></i></a>
+           
             <ul className='navUl' id='navUl'> 
                 
                 <li><a className='navLi'onClick={()=>handlescroll("home")} >Home</a></li>
