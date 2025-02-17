e1d0622a6f459338d06b1b3ec85cca18c81a97d1
Dinithi1625403 <dinithi1625403@gmail.com>
2025-02-17 08:39:10 +0530

    modified aboutsection-achivements

--- src/components/about/achivements.jsx
+++ src/components/about/achivements.jsx
@@ -1,9 +1,21 @@
-import './education.css';
+import './achivements.css';
 
 export default function Achivements(){
+    const AchivementsData = [
+        {title:"Semifinalist of hack X 9.0 2024",description:"HackX is a Ideathon organized by Faculty of Science,University of Kaleniya.",image:"https://i.ibb.co/7zvJYJ1/hack-X-9-0.png"},
+        {title:"Finalist of InspiHER",description:"InspiHER is a Hackathon organized by IEEE WIE AG IEEE SL Section.",image:"https://i.ibb.co/7zvJYJ1/hack-X-9-0.png"},
+    ];
     return(
         <>
-            <p>Hello there</p>
+            <div className="achivement-container">
+                {AchivementsData.map((item, index) => (
+                    <div key={index} className="achivement-card">
+                        <h6 className="achivement-title">{item.title}</h6>
+                        <p className="achivement-description">{item.description}</p>
+                        <img src={item.image} alt={item.title} className="achivement-image" />
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
